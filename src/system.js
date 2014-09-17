21bf6e301d8f9e985e827572be30987763229764
Vince Allen <vince@spotify.com>
2014-09-17 13:13:44 -0400

    No longer overrides Burner.System.

--- src/system.js
+++ src/system.js
@@ -1,9 +1,19 @@
+/*global window, document */
+/*jshint supernew:true */
+
 var Item = require('./item');
 var FPSDisplay = require('fpsdisplay');
-var System = require('burner').System;
 var Utils = require('burner').Utils;
 var Vector = require('burner').Vector;
 var World = require('./world');
+
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+/** @namespace */
+var System = {
+  name: 'System'
+};
 
 /**
  * Holds additional classes that can be defined at runtime.
@@ -12,6 +22,58 @@ var World = require('./world');
 System.Classes = {
   'Item': Item
 };
+
+/**
+ * Holds a vector describing the system gravity.
+ * @memberof System
+ */
+System.gravity = new Vector(0, 1);
+
+/**
+ * Holds a vector describing the system wind.
+ * @memberof System
+ */
+System.wind = new Vector();
+
+/**
+ * Stores references to all items in the system.
+ * @memberof System
+ * @private
+ */
+System._records = [];
+
+/**
+ * Stores references to all items removed from the system.
+ * @memberof System
+ * @private
+ */
+System._pool = [];
+
+/**
+ * Holds the current and last mouse/touch positions relative
+ * to the browser window. Also, holds the current mouse velocity.
+ * @public
+ */
+System.mouse = {
+  location: new Vector(),
+  lastLocation: new Vector(),
+  velocity: new Vector()
+};
+
+/**
+ * Increments with each call to System.loop.
+ * @type {number}
+ * @private
+ */
+System.clock = 0;
+
+/**
+ * System.loop() calls this function. Use to execute
+ * a function in the animation loop outside of any items.
+ * @type {Function}
+ * @private
+ */
+System.frameFunction = null;
 
 /**
  * Stores references to all buffers in the system.
@@ -118,6 +180,35 @@ System.getAllBuffers = function() {
   return System._buffers;
 };
 
+ /**
+  * Call to execute any setup code before starting the animation loop.
+  * @function setup
+  * @param  {Object} opt_func   A function to run before the function exits.
+  * @memberof System
+  */
+System.setup = function(opt_func) {
+
+  var func = opt_func || function() {}, i, l, max;
+
+  document.body.onorientationchange = System.updateOrientation;
+
+  // save the current and last mouse position
+  Utils.addEvent(document, 'mousemove', System._recordMouseLoc);
+
+  // save the current and last touch position
+  Utils.addEvent(window, 'touchstart', System._recordMouseLoc);
+  Utils.addEvent(window, 'touchmove', System._recordMouseLoc);
+  Utils.addEvent(window, 'touchend', System._recordMouseLoc);
+
+  // listen for key up
+  Utils.addEvent(window, 'keyup', System._keyup);
+
+  // save the setup callback in case we need to reset the system.
+  System.setupFunc = func;
+
+  System.setupFunc.call(this);
+};
+
 /**
  * Adds instances of class to _records and calls init on them.
  * @function add
@@ -156,6 +247,41 @@ System.add = function(opt_klass, opt_options, opt_world) {
   obj.init(world, options);
   records.push(obj);
   return obj;
+};
+
+/**
+ * Removes all properties from the passed object.
+ * @param  {Object} obj An object.
+ * @return {Object}     The passed object.
+ */
+System._cleanObj = function(obj) {
+  for (var prop in obj) {
+    if (obj.hasOwnProperty(prop)) {
+      delete obj[prop];
+    }
+  }
+  return obj;
+};
+
+/**
+ * Removes an item from the system.
+ * @function remove
+ * @memberof System
+ * @param {Object} obj The item to remove.
+ */
+System.remove = function (obj) {
+
+  var i, max, records = System._records;
+
+  for (i = 0, max = records.length; i < max; i++) {
+    if (records[i].id === obj.id) {
+      if (records[i].el) {
+        records[i].el.style.visibility = 'hidden'; // hide item
+      }
+      System._pool[System._pool.length] = records.splice(i, 1)[0]; // move record to pool array
+      break;
+    }
+  }
 };
 
 /**
@@ -402,6 +528,119 @@ System._buildStringRGBA = function(item) {
         (resolution * item.scale) + 'px ' + // spread
         'rgba(' + item.color[0] + ',' + item.color[1] + ',' + item.color[2] + // color
         ', ' + item.opacity + '),'; // opacity
+};
+
+/**
+ * Saves the mouse/touch location relative to the browser window.
+ *
+ * @function _recordMouseLoc
+ * @memberof System
+ * @private
+ */
+System._recordMouseLoc = function(e) {
+
+  var touch, world = System.firstWorld();
+
+  System.mouse.lastLocation.x = System.mouse.location.x;
+  System.mouse.lastLocation.y = System.mouse.location.y;
+
+  if (e.changedTouches) {
+    touch = e.changedTouches[0];
+  }
+
+  /**
+   * Mapping window size to world size allows us to
+   * lead an agent around a world that's not bound
+   * to the window.
+   */
+  if (e.pageX && e.pageY) {
+    System.mouse.location.x = Utils.map(e.pageX, 0, window.innerWidth, 0, world.width);
+    System.mouse.location.y = Utils.map(e.pageY, 0, window.innerHeight, 0, world.height);
+  } else if (e.clientX && e.clientY) {
+    System.mouse.location.x = Utils.map(e.clientX, 0, window.innerWidth, 0, world.width);
+    System.mouse.location.y = Utils.map(e.clientY, 0, window.innerHeight, 0, world.height);
+  } else if (touch) {
+    System.mouse.location.x = touch.pageX;
+    System.mouse.location.y = touch.pageY;
+  }
+
+  System.mouse.velocity.x = System.mouse.lastLocation.x - System.mouse.location.x;
+  System.mouse.velocity.y = System.mouse.lastLocation.y - System.mouse.location.y;
+};
+
+/**
+ * Returns the first world in the system.
+ *
+ * @function firstWorld
+ * @memberof System
+ * @returns {null|Object} An instance of World.
+ */
+System.firstWorld = function() {
+  return this._records.length ? this._records[0] : null;
+};
+
+/**
+ * Returns all worlds.
+ *
+ * @function allWorlds
+ * @memberof System
+ * @return {Array.<World>} An array of worlds.
+ */
+System.allWorlds = function() {
+  return System.getAllItemsByName('World');
+};
+
+/**
+ * Returns an array of items created from the same constructor.
+ *
+ * @function getAllItemsByName
+ * @memberof System
+ * @param {string} name The 'name' property.
+ * @param {Array} [opt_list = this._records] An optional list of items.
+ * @returns {Array} An array of items.
+ */
+System.getAllItemsByName = function(name, opt_list) {
+
+  var i, max, arr = [],
+      list = opt_list || this._records;
+
+  for (i = 0, max = list.length; i < max; i++) {
+    if (list[i].name === name) {
+      arr[arr.length] = list[i];
+    }
+  }
+  return arr;
+};
+
+/**
+ * Returns an array of items with an attribute that matches the
+ * passed 'attr'. If 'opt_val' is passed, 'attr' must equal 'val'.
+ *
+ * @function getAllItemsByAttribute
+ * @memberof System
+ * @param {string} attr The property to match.
+ * @param {*} [opt_val=] The 'attr' parameter must equal this param.
+ * @param {string} name The item's name property must equal this param.
+ * @returns {Array} An array of items.
+ */
+System.getAllItemsByAttribute = function(attr, opt_val, opt_name) { // TODO: add test
+
+  var i, max, arr = [], records = this._records,
+      val = typeof opt_val !== 'undefined' ? opt_val : null,
+      name = opt_name || false;
+
+  for (i = 0, max = records.length; i < max; i++) {
+    if (typeof records[i][attr] !== 'undefined') {
+      if (val !== null && records[i][attr] !== val) {
+        continue;
+      }
+      if (name && records[i].name !== name) {
+        continue;
+      }
+      arr[arr.length] = records[i];
+    }
+  }
+  return arr;
 };
 
 /**
